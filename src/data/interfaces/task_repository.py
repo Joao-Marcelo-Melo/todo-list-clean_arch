be7955744fdc802f4068e92bb0df17eb74fdfe86
Joao-Marcelo-Melo <marcelo25256464@gmail.com>
2023-11-19 14:04:13 -0300

    feat: implemmenting repository interface

---
+++ src/data/interfaces/task_repository.py
@@ -0,0 +1,21 @@
+from typing import List
+from abc import ABC, abstractmethod
+from src.domain.models.task import Task
+
+class TaskRepositoryInterface(ABC):
+
+    @abstractmethod
+    def insert_task(self, title : str , description : str, status : bool) -> Task:
+        pass
+
+    @abstractmethod
+    def get_task_by_id(self, task_id : int) -> List[Task]:
+        pass
+
+    @abstractmethod
+    def get_all_tasks(self) -> List[Task]:
+        pass
+
+    @abstractmethod
+    def delete_task(self, task_id : int) -> None:
+        pass
