800009aa03713c03e3ce82981a6a7c593b1a8fc4
Joao-Marcelo-Melo <marcelo25256464@gmail.com>
2023-11-19 14:02:07 -0300

    feat: adding title to schema.sql

--- init/schema.sql
+++ init/schema.sql
@@ -1,7 +1,8 @@
-CREATE DATABASE todo_list
+CREATE DATABASE todo_list;
 
 CREATE TABLE tasks (
     id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
     description VARCHAR(255) NOT NULL,
     status BOOLEAN NOT NULL
 );
