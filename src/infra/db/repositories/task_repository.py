06b9a57f6933412c66d7f0e0f4b3c86ecd481cb2
Joao-Marcelo-Melo <marcelo25256464@gmail.com>
2023-11-19 14:10:11 -0300

    feat: implemmenting insert_task in repository_Db

---
+++ src/infra/db/repositories/task_repository.py
@@ -0,0 +1,35 @@
+from typing import List
+from src.infra.db.settings.connection import DBConnectionHandler
+from src.infra.db.entities.tasks import Tasks as TasksEntity
+from src.data.interfaces.task_repository import TaskRepositoryInterface
+from src.domain.models.task import Task
+from src.infra.env.env_config import GetDbEnviroments
+
+class TasksRepository(TaskRepositoryInterface):
+
+    @classmethod
+    def insert_task(cls, title : str, description : str, status=False) -> Task:
+        with DBConnectionHandler(GetDbEnviroments()) as database:
+            try:
+                new_task = TasksEntity(
+                    title=title,
+                    description=description,
+                    status=status
+                )
+                database.session.add(new_task)
+                database.session.commit()
+            except Exception as error:
+                database.session.rollback()
+                raise error
+
+    @classmethod
+    def get_task_by_id(cls, task_id : int) -> List[Task]:
+        raise NotImplementedError('nao implementado')
+
+    @classmethod
+    def get_all_tasks(cls) -> List[Task]:
+        raise NotImplementedError('nao implementado')
+
+    @classmethod
+    def delete_task(cls, task_id : int) -> None:
+        raise NotImplementedError('nao implementado')
